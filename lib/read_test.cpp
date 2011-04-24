bcc69726f4593fe432353f4c579905e29b451811
Richard Allen <rsaxvc@gmail.com>
2011-04-24 11:19:02 -0500

    cloned main test to read_test and write test

---
+++ lib/read_test.cpp
@@ -0,0 +1,58 @@
+#include "async_file.h"
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <cstdlib>
+#include <fcntl.h>
+
+using namespace std;
+int main(int numArgs, char * args[] )
+{{
+async_file f;
+int i;
+
+if( numArgs < 2 )
+	{
+	cout<<"Usage: "<<args[0]<<" <filename> "<<endl;
+	exit(1);
+	}
+
+if( !f.do_open( args[1], O_RDWR | O_CREAT ) )
+	{
+	cout<<"Error opening "<<args[1]<<endl;
+	exit(2);
+	}
+
+f.print_backend();
+
+cout
+	<<"fstat:"<<f.get_status()
+	<<"\nfsize:"<<f.get_size()
+	<<endl;
+
+i=0;
+
+f.print_backend();
+
+for( int j = 0; j < f.get_size(); ++j )
+	{
+	char buf;
+	cout<<endl;
+	if( f.do_read( &buf, j, sizeof( buf ) ) != 1 )
+		{
+		cout<<"Error reading file at offset:"<<j<<endl;
+		exit(4);
+		}
+	cout<<j<<"="<<hex<<(unsigned int)buf<<endl;
+	}
+
+f.print_backend();
+
+cout<<f.get_stats()<<endl;
+//f.do_write( kbuf, 0, sizeof( kbuf ) );
+f.do_close();
+
+}
+return 0;
+}
